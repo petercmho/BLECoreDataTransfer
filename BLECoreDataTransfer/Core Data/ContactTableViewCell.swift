6b97b43c704c66d2da14fd4b920b068d1ba23a64
Peter@work <pho@douglaslightingcontrols.com>
2017-04-07 10:53:45 -0700

    Completed reading NSFetchedResultsController implementation. Added ContactTableViewCell.

---
+++ BLECoreDataTransfer/Core Data/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  BLECoreDataTransfer
+//
+//  Created by Peter Ho on 2017-04-07.
+//  Copyright © 2017 Peter Ho. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
